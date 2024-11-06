7c7ca5646ccce3b77c8868325c515fcc3aa27fba
bahaaaaaaaaa <bhavya.raturi.048@my.csun.edu>
2024-11-05 23:25:56 -0800

    Create/Footer Modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import {BrowserRouter, Routes,Route} from 'react-router-dom'
 import  supabase  from './utils/supabase.js'
 import './App.css';
