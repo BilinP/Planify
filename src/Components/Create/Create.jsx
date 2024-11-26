1af58f5de8ef57db3c538470b42a7acbb5c3d98f
bahaaaaaaaaa <bhavya.raturi.048@my.csun.edu>
2024-11-25 21:40:53 -0800

    update create changes

--- src/Components/Create/Create.jsx
+++ src/Components/Create/Create.jsx
@@ -1,16 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import Footer from '../Footer/Footer.jsx';
 import './Create.css';
+import headerImage from '../../assets/header-image.webp';
+
 
 const Create = () => {
     const [showOtherInput, setShowOtherInput] = useState(false);
     const [eventType, setEventType] = useState('');
     const [eventDate, setEventDate] = useState('');
+    const [eventTitle, setEventTitle] = useState('');
+    const [eventSummary, setEventSummary] = useState('');
+    const [eventLocation, setEventLocation] = useState('');
+    const [isOnline, setIsOnline] = useState(false);
 
     const handleEventChange = (e) => {
         const selectedEvent = e.target.value;
         setEventType(selectedEvent);
         setShowOtherInput(selectedEvent === 'other');
+    }
+
+    const handleLocationChange = (e) => {
+        const selectedLocation = e.target.value;
+        setEventType(selectedLocation);
+        setIsOnline(selectedLocation === 'online');
     }
 
     useEffect(() => {
@@ -29,81 +40,94 @@ const Create = () => {
     }
 
     return (
-        <div className='container'>
-            <div className="header">
-                <div className='text'>Event Creator</div>
-                <div className="underline"></div>
-            </div>
-            <div className="inputs">
-                <div className="input">
-                    <img className="person" src="https://png.pngtree.com/png-clipart/20240107/original/pngtree-holiday-concept-event-on-chalkboard-play-write-drawing-photo-png-image_14048196.png" alt="" />
-                    <select className="event-dropdown" value={eventType} onChange={handleEventChange}>
-                        <option value="" disabled>Select Event Type</option>
-                        <option value="anniversary">Anniversary</option>
-                        <option value="baby-shower">Baby Shower</option>
-                        <option value="birthday">Birthday</option>
-                        <option value="conference">Conference</option>
-                        <option value="convention">Convention</option>
-                        <option value="festival">Festival</option>
-                        <option value="fundraiser">Fundraiser</option>
-                        <option value="gala">Gala</option>
-                        <option value="meeting">Meeting</option>
-                        <option value="networking">Networking</option>
-                        <option value="non-profit">Non-Profit Event</option>
-                        <option value="open-house">Open House</option>
-                        <option value="party">Party</option>
-                        <option value="performance">Performance</option>
-                        <option value="professional-event">Professional Event</option>
-                        <option value="retreat">Retreat</option>
-                        <option value="reunion">Reunion</option>
-                        <option value="screening">Screening</option>
-                        <option value="seminar">Seminar</option>
-                        <option value="sporting-event">Sporting Event or Competition</option>
-                        <option value="tasting">Tasting</option>
-                        <option value="tour">Tour</option>
-                        <option value="trip">Trip</option>
-                        <option value="wedding">Wedding</option>
-                        <option value="workshop">Workshop or Class</option>
-                        <option value="other">Other</option>
-                    </select>
-                </div>
+        <div className="create-container">
+            <img 
+                src={headerImage}
+                alt="eventBanner" 
+                className="header-image" 
+            />
 
-                {showOtherInput && (
-                    <div className="input">
-                        <input type="text" placeholder="Please specify your event type" />
-                    </div>
-                )}
-
-                <div className="input">
-                    <img className="email" src="https://cdn.iconscout.com/icon/free/png-256/free-email-2026367-1713640.png" alt="" />
-                    <input type="email" placeholder="Email" />
-                </div>
-
-                <div className="input">
-                    <img className="capacity" src="https://png.pngtree.com/png-clipart/20240107/original/pngtree-holiday-concept-event-on-chalkboard-play-write-drawing-photo-png-image_14048196.png" alt="" />
-                    <input type="text" placeholder="Number of Attendee's" />
-                </div>
-
-                <div className="input">
-                    <img className="date1" src="https://www.clipartmax.com/png/full/31-316421_calendar-computer-icons-clip-art-calendar-date-icon.png" alt="" />
+            {/* Event Name Box */}
+            <div className="event-name-box">
+                <div className="event-name-header">
+                    <h3>Event Title</h3>
+                    <div className="underline"></div>
                     <input 
-                        id="dateInput" 
-                        type="date" 
-                        value={eventDate}
-                        min={eventDate}
-                        onChange={handleDateChange} 
-                        placeholder="Date" 
+                        type="text" 
+                        className="event-title-input" 
+                        placeholder="Name" 
+                        value={eventTitle} 
+                        onChange={(e) => setEventTitle(e.target.value)} 
                     />
                 </div>
+            </div>
 
-                <div className="createButton">
-                    <div className="createBut" onClick={handleSubmit}>Create!</div>
+            {/* Event Date Box */}
+            <div className="event-date-box">
+                <div className="event-date-time">
+                    <h3>Date and Time</h3>
+                    <div className="underline"></div>
+                    <div className="event-date">
+                        <input 
+                            id="dateInput" 
+                            type="date" 
+                            value={eventDate} 
+                            min={eventDate} 
+                            onChange={handleDateChange} 
+                            placeholder="Date" 
+                        />
+                    </div>
                 </div>
+            </div> 
 
-                <div className="agreement-checkbox">
-                    <input type="checkbox" id="agreeToTerms" required />
-                    <label htmlFor="agreeToTerms">I agree to the Terms and Conditions</label>
+            {/* Event Location Box */}
+            <div className='event-location-box'>
+                <div className="event-location">
+                    <h3>Location</h3>
+                    <div className="underline"></div>
+                    <div className="location-info">
+                        <select 
+                            className="location-dropdown" 
+                            value={eventType} 
+                            onChange={handleLocationChange}
+                        >                        
+                            <option value="" disabled>Select Location Type</option>
+                            <option value="online">Online</option>
+                            <option value="in-person">In Person</option>
+                        </select>
+                    </div>
+                    {!isOnline && (
+                        <div className="location-info">
+                            <input 
+                                type="text" 
+                                placeholder="Event Location" 
+                                value={eventLocation} 
+                                onChange={(e) => setEventLocation(e.target.value)}
+                                className="event-location-input"
+                            />
+                        </div>
+                    )}
                 </div>
+            </div>
+
+            {/* About Event Box */}
+            <div className='about-event-box'>
+                <div className='event-about'>
+                    <h3>About Event</h3>
+                    <div className="underline"></div>
+                    <input 
+                        type='text' 
+                        className='event-about-input' 
+                        placeholder='Description' 
+                        value={eventSummary} 
+                        onChange={(e) => setEventSummary(e.target.value)} 
+                    />
+                </div>
+            </div>
+
+            {/* Create Button Creation */}
+            <div className='create-button-box'>
+                <button className='create-button' onClick={handleSubmit}>Create</button>
             </div>
         </div>
     )
